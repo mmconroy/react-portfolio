8c206f58cd7f37757730f4e93af14c2aee10f4d8
Matthew Conroy <mmconroy@gmail.com>
2020-07-21 21:21:14 -0400

    Removed Switch from routing

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route exact path="About">
-          <About />
-        </Route>
-      </Switch>
+
+      <Route exact path="/">
+        <Main />
+      </Route>
+      <Route exact path="About">
+        <About />
+      </Route>
     </div>
   );
 }
